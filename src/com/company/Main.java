25e660edb8dec5f72379f81bc3e599bffe66d437
Aline Sarter <55752648+sarterA@users.noreply.github.com>
2020-11-11 18:17:05 +0100

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,18 +4,29 @@ import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
 
-        //para executar cada actividade
-//        conditionals.exercici1;
-//        public static void exercici1(){}
-
-
-
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
